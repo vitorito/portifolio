dec0b176bf65186cde2b8ef1103a9835b7e73a06
vitorito <victorolivbati@gmail.com>
2022-09-23 09:40:00 -0300

    refactor menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,18 @@
 import './projects.js';
 import './skills.js';
 
+const closeMenu = () => menu.classList.remove('open');
+
+const toggleMenu = () => {
+  menu.classList.toggle('open');
+}
+
 const menu = document.querySelector('.menu');
+const menuBtn = document.querySelector('.menu__btn');
 
-const toggleMenu = () => menu.classList.toggle('open');
+menuBtn.addEventListener('click', (e) => {
+  e.stopPropagation();
+  toggleMenu();
+});
 
-
-menu.addEventListener('click', toggleMenu);
+menu.addEventListener('click', closeMenu);
